ea6c4729aef7250df7098427e13eab20f37156a6
omega-jean <jeanalex1405@gmail.com>
2024-09-25 14:11:08 +0200

    task

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
-// task 2
+// task 4
 interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
@@ -7,11 +7,11 @@ interface Teacher {
     location: string;
     [key: string]: any;
   }
-
+  
   interface Directors extends Teacher {
     numberOfReports: number;
   }
-
+  
   const director1: Directors = {
     firstName: 'John',
     lastName: 'Doe',
@@ -21,3 +21,46 @@ interface Teacher {
   };
   
   console.log(director1);
+
+  interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+  };
+  
+  console.log(printTeacher("John", "Doe")); // J. Doe
+
+  interface StudentClassConstructor {
+    new (firstName: string, lastName: string): StudentClass;
+  }
+  
+  interface StudentClass {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  class Student implements StudentClass {
+    firstName: string;
+    lastName: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
+  }
+
+  const student1 = new Student('Jane', 'Doe');
+  console.log(student1.displayName()); // Jane
+  console.log(student1.workOnHomework()); // Currently working
